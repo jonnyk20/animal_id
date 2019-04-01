0397578f7e03872db1131abbafd0c6ff7031f20c
Jonny Kalambay <jonnyk20@gmail.com>
2019-04-01 18:47:53 +0000

    Refactoring camera to different component

---
+++ lib/camera.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class Camera extends StatefulWidget {
+  final CameraDescription camera;
+
+  Camera(this.camera);
+
+  @override
+  _CameraState createState() => _CameraState(camera);
+}
+
+class _CameraState extends State<Camera> {
+  final CameraDescription camera;
+  CameraController controller;
+
+  _CameraState(this.camera);
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(camera, ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller));
+  }
+}
